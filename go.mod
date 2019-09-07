b0cbb0587289c8cd78a81837da127655bf1283f3
Walter White <WalterWhite@walterjwhite.com>
2019-09-07 11:42:55 -0400

    build libraries - helpers

---
+++ go.mod
@@ -0,0 +1,6 @@
+module github.com/walterjwhite/go-application
+
+require (
+	github.com/BurntSushi/xgb v0.0.0-20160522181843-27f122750802 // indirect
+	github.com/vova616/screenshot v0.0.0-20190211115255-f9547e483e0a
+)
---
+++ libraries/screenshot/screenshot.go
@@ -0,0 +1,25 @@
+package screenshot
+
+import (
+	"github.com/vova616/screenshot"
+	"image/png"
+	"libraries/path"
+	"log"
+)
+
+// TODO: currently only taking PNG screenshots
+// support other formats
+
+func Take(label string, detail string) {
+	img, err := screenshot.CaptureScreen()
+	if err != nil {
+		panic(err)
+	}
+
+	file := path.GetFile(label, "png", detail)
+
+	defer file.Close()
+	png.Encode(file, img)
+
+	log.Printf("Captured screenshot: %v / %v", label, file.Name())
+}
