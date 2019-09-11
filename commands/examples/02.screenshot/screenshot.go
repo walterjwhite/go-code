f65b43345b0cd42253ab5e802d77f044dc4c97cb
Walter White <WalterWhite@walterjwhite.com>
2019-09-10 20:14:59 -0400

    added CLI arguments

--- commands/examples/02.screenshot/screenshot.go
+++ commands/examples/02.screenshot/screenshot.go
@@ -1,17 +1,29 @@
 package main
 
 import (
+	"flag"
 	"github.com/walterjwhite/go-application/libraries/application"
+
 	"github.com/walterjwhite/go-application/libraries/path"
 	"github.com/walterjwhite/go-application/libraries/screenshot"
+	"log"
 )
+
+var label = flag.String("Label", "", "Screenshot label")
+var detail = flag.String("Detail", "", "Screenshot detail")
 
 func main() {
 	application.Configure()
 	defer application.OnCompletion()
 
-	//application.Wait(ctx)
+	if len(*label) == 0 {
+		log.Fatal("Please specify a label for the screenshot")
+	}
+
+	if len(*detail) == 0 {
+		log.Fatal("Please specify a detailed message for the screenshot")
+	}
 
 	path.WithSessionDirectory("~/.audit")
-	screenshot.Take("label", "detail")
+	screenshot.Take(*label, *detail)
 }
