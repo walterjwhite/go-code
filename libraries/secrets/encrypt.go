11c53266b2f82e62cbcb4f394052a066b6804fc0
Walter White <WalterWhite@walterjwhite.com>
2019-09-20 23:07:00 -0400

    encrypt/put secrets

---
+++ libraries/secrets/encrypt.go
@@ -0,0 +1,79 @@
+package secrets
+
+import (
+	"github.com/walterjwhite/go-application/libraries/logging"
+	"log"
+	"os"
+	"os/exec"
+	"time"
+)
+
+const DateTimeLayout = "2006/01/02 15:04:05"
+
+func Encrypt(name *string, message *string, data []byte) {
+	log.Printf("processing secret: %v\n", *name)
+
+	secretPath := getSecretPath(name)
+	secretValuePath := secretPath + "/value"
+
+	SecretsConfigurationInstance.EncryptionConfiguration.EncryptFile(secretValuePath, data)
+
+	log.Printf("Stored secret in %v (%v)\n", secretValuePath, len(data))
+
+	putLastUpdated(secretPath)
+	commit(secretPath, message)
+}
+
+func getSecretPath(name *string) string {
+	secretPath := SecretsConfigurationInstance.RepositoryPath + "/" + *name
+	logging.Panic(os.MkdirAll(secretPath, 0755))
+
+	return secretPath
+}
+
+func putLastUpdated(secretPath string) {
+	secretLastUpdatedPath := secretPath + "/last-updated"
+
+	f, err := os.Create(secretLastUpdatedPath)
+	logging.Panic(err)
+
+	defer f.Close()
+
+	lastUpdated := getDateTimeLastUpdated()
+	_, err = f.Write(lastUpdated)
+	logging.Panic(err)
+
+	log.Printf("Stored last updated in %v (%v)\n", secretLastUpdatedPath, lastUpdated)
+}
+
+func getDateTimeLastUpdated() []byte {
+	t := time.Now()
+	formattedDateTime := t.Format(DateTimeLayout)
+
+	return []byte(formattedDateTime)
+}
+
+func commit(secretPath string, message *string) {
+	cmd := exec.Command("git", "add", secretPath)
+	cmd.Dir = SecretsConfigurationInstance.RepositoryPath
+
+	stdoutStderr, err := cmd.CombinedOutput()
+	log.Printf("%s\n", stdoutStderr)
+
+	logging.Panic(err)
+
+	cmd = exec.Command("git", "commit", secretPath, "-m", *message)
+	cmd.Dir = SecretsConfigurationInstance.RepositoryPath
+	stdoutStderr, err = cmd.CombinedOutput()
+	log.Printf("%s\n", stdoutStderr)
+
+	logging.Panic(err)
+
+	cmd = exec.Command("git", "push")
+	cmd.Dir = SecretsConfigurationInstance.RepositoryPath
+
+	stdoutStderr, err = cmd.CombinedOutput()
+	logging.Panic(err)
+
+	log.Printf("Added secret:\n%s\n", stdoutStderr)
+}
--- libraries/secrets/go.mod
+++ libraries/secrets/go.mod
@@ -4,7 +4,7 @@ go 1.12
 
 require (
 	github.com/mitchellh/go-homedir v1.1.0
-	github.com/walterjwhite/go-application/libraries/encryption v0.0.0-20190921022528-50acf6f9f0bb
-	github.com/walterjwhite/go-application/libraries/logging v0.0.0-20190921022528-50acf6f9f0bb
-	github.com/walterjwhite/go-application/libraries/yamlhelper v0.0.0-20190921022528-50acf6f9f0bb
+	github.com/walterjwhite/go-application/libraries/encryption v0.0.0-20190921023002-edcf3bf8ee52
+	github.com/walterjwhite/go-application/libraries/logging v0.0.0-20190921023002-edcf3bf8ee52
+	github.com/walterjwhite/go-application/libraries/yamlhelper v0.0.0-20190921023002-edcf3bf8ee52
 )
