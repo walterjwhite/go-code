d7014e7d1f94132c076e79f5dc7202ef383b6d10
Walter White <WalterWhite@walterjwhite.com>
2019-09-28 00:29:22 -0400

    +heartbeat

---
+++ libraries/heartbeat/go.mod
@@ -0,0 +1,8 @@
+module github.com/walterjwhite/go-application/libraries/heartbeat
+
+go 1.12
+
+require (
+	github.com/walterjwhite/go-application/libraries/logging v0.0.0-20190928041326-6a1a4d3f8ed1 // indirect
+	github.com/walterjwhite/go-application/libraries/periodic v0.0.0-20190928041326-6a1a4d3f8ed1
+)
---
+++ libraries/heartbeat/heartbeat.go
@@ -0,0 +1,31 @@
+package heartbeat
+
+import (
+	"context"
+	"time"
+
+	"github.com/walterjwhite/go-application/libraries/periodic"
+)
+
+type HeartbeatInstance struct {
+	Interval time.Duration
+
+	// Function to invoke every interval
+	HeartbeatFunction func() error
+
+	// Long-running function
+	Function func()
+}
+
+func Heartbeat(function func(), heartbeatFunction func() error, interval time.Duration) {
+	h := &HeartbeatInstance{Interval: interval, HeartbeatFunction: heartbeatFunction, Function: function}
+	h.Call()
+}
+
+func (h *HeartbeatInstance) Call() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	periodic.Periodic(ctx, h.Interval, h.HeartbeatFunction)
+	h.Function()
+}
