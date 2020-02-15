e3452d54a447d7b7ab88ad15fc150b9eb84e50a9
Walter White <WalterWhite@walterjwhite.com>
2020-02-15 06:16:59 -0500

    merged

--- libraries/wait/wait.go
+++ libraries/wait/wait.go
@@ -36,44 +36,6 @@ func (w *waitInstance) doWait() {
 	close(w.channel)
 }
 
-package wait
-
-import (
-	"context"
-	"time"
-
-	"github.com/rs/zerolog/log"
-	"github.com/walterjwhite/go-application/libraries/logging"
-	"github.com/walterjwhite/go-application/libraries/periodic"
-	"github.com/walterjwhite/go-application/libraries/timeout"
-)
-
-type waitInstance struct {
-	periodic *periodic.PeriodicInstance
-	function func() bool
-
-	channel chan bool
-}
-
-// calls the function periodically with the given interval until it returns true, the call times out, or the context is Done
-func Wait(ctx context.Context, interval *time.Duration, limit *time.Duration, userFunction func() bool) {
-	channel := make(chan bool, 1)
-
-	wctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-
-	w := &waitInstance{channel: channel, function: userFunction}
-	w.periodic = periodic.Periodic(wctx, interval, w.monitorFunction)
-
-	// wait until done
-	logging.Panic(timeout.Limit(w.doWait, limit, wctx))
-}
-
-func (w *waitInstance) doWait() {
-	<-w.channel
-	close(w.channel)
-}
-
 func (w *waitInstance) cancel() {
 	w.periodic.Cancel()
 }
