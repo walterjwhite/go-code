69a1468a51619a04730f20cf88352776add50c30
Walter White <WalterWhite@walterjwhite.com>
2025-05-16 00:00:00 -0400

    default - merged

--- lib/application/logging.go
+++ lib/application/logging.go
@@ -17,18 +17,19 @@ const (
 var (
   logLevel  = flag.String("log-level", "warn", "log level")
   logTarget = flag.String("log-target", "", "log file, if empty, stderr is used, if SYSLOG, syslog is used")
+  logWriter io.WriteCloser
 )
 
 func configureLogging() {
   zerolog.TimeFieldFormat = logDateTimeFormat
 
-  var f = getWriter()
-  log.Logger = zerolog.New(zerolog.SyncWriter(f)).With().Timestamp().Logger()
+  logWriter = getWriter()
+  log.Logger = zerolog.New(zerolog.SyncWriter(logWriter)).With().Timestamp().Logger()
 
   setLogLevel()
 }
 
-func getWriter() io.Writer {
+func getWriter() io.WriteCloser {
   if len(*logTarget) > 0 {
     if *logTarget == "SYSLOG" {
       syslogger, err := syslog.New(syslog.LOG_KERN|syslog.LOG_EMERG|syslog.LOG_ERR|syslog.LOG_INFO|syslog.LOG_CRIT|syslog.LOG_WARNING|syslog.LOG_NOTICE|syslog.LOG_DEBUG, ApplicationName)
@@ -54,16 +55,6 @@ func prepareFile() io.WriteCloser {
   var f io.WriteCloser
   f, err := os.OpenFile(*logTarget, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
   logging.Panic(err)
-  defer func() {
-    logging.Panic(f.Close())
-  }()
-
-/*
-  f = zlib.NewWriter(f)
-  defer func() {
-    logging.Panic(f.Close())
-  }()
-*/
 
   return f
 }
