f99d591c6480a1f55019768de64e9cf3558b312c
Walter White <WalterWhite@walterjwhite.com>
2026-03-02 09:45:30 -0500

    merged

--- cmd/utils/google-pubsub/publish-cmd/main.go
+++ cmd/utils/google-pubsub/publish-cmd/main.go
@@ -23,12 +23,6 @@ var (
 
 	functionName = flag.String("func", "", "function to execute")
 	arguments    = flag.String("args", "", "arguments to pass function, optional")
-
-	allowedFunctions = map[string]bool{
-		"process":   true,
-		"publish":   true,
-		"subscribe": true,
-	}
 )
 
 func init() {
@@ -40,14 +34,9 @@ func init() {
 
 func main() {
 	defer application.OnPanic()
-	flag.Parse()
 
 	if len(*functionName) == 0 {
 		logging.Error(errors.New("expecting command to be non-empty"))
-	}
-
-	if !isAllowedFunction(*functionName) {
-		logging.Error(errors.New("function not in allowed list"))
 	}
 
 	c := exec.Cmd{FunctionName: *functionName}
@@ -62,8 +51,4 @@ func main() {
 	}
 
 	logging.Warn(publisherConfiguration.GoogleConf.Publish(publisherConfiguration.TopicName, jsonString), "main")
-}
-
-func isAllowedFunction(functionName string) bool {
-	return allowedFunctions[functionName]
 }
--- lib/application/property/cli.go
+++ lib/application/property/cli.go
@@ -2,14 +2,13 @@ package property
 
 import (
 	"flag"
-	"os"
 
 	"github.com/urfave/sflags/gen/gflag"
 	"github.com/walterjwhite/go-code/lib/application/logging"
 )
 
 func LoadCli(config any) {
-	fs := flag.NewFlagSet("cli", flag.ContinueOnError)
+	fs := flag.CommandLine 
 	logging.Warn(gflag.ParseTo(config, fs), "LoadCli")
-	logging.Warn(fs.Parse(os.Args[1:]), "fs.Parse")
+	flag.Parse()
 }
