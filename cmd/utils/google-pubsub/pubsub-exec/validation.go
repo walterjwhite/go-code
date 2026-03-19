cbd3d615984c668a629f8928cab58611f0ff98f6
Walter White <WalterWhite@walterjwhite.com>
2026-03-19 11:52:19 -0400

    merged

--- cmd/utils/google-pubsub/pubsub-exec/validation.go
+++ cmd/utils/google-pubsub/pubsub-exec/validation.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"net/url"
 	"regexp"
+	"strings"
 	"unicode"
 )
 
@@ -19,8 +21,66 @@ func isValidArgument(arg string) bool {
 		return false
 	}
 
+	if looksLikeURL(arg) {
+		return isValidYouTubeURL(arg)
+	}
+
 	for _, r := range arg {
 		if !isValidCharacter(r) {
+			return false
+		}
+	}
+	return true
+}
+
+func looksLikeURL(arg string) bool {
+	lower := strings.ToLower(arg)
+	return strings.Contains(lower, "http://") || strings.Contains(lower, "https://")
+}
+
+func isValidYouTubeURL(raw string) bool {
+	parsed, err := url.ParseRequestURI(raw)
+	if err != nil {
+		return false
+	}
+
+	if parsed.Scheme != "https" {
+		return false
+	}
+
+	host := strings.ToLower(parsed.Hostname())
+	allowedHosts := map[string]bool{
+		"youtube.com":     true,
+		"www.youtube.com": true,
+		"youtu.be":        true,
+		"m.youtube.com":   true,
+	}
+	if !allowedHosts[host] {
+		return false
+	}
+
+	if parsed.Fragment != "" {
+		return false
+	}
+
+	for key, values := range parsed.Query() {
+		if !isValidURLComponent(key) {
+			return false
+		}
+		for _, v := range values {
+			if !isValidURLComponent(v) {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func isValidURLComponent(s string) bool {
+	for _, r := range s {
+		if !unicode.IsLetter(r) && !unicode.IsDigit(r) &&
+			r != '-' && r != '_' && r != '.' && r != '~' {
 			return false
 		}
 	}
--- go.mod
+++ go.mod
@@ -69,7 +69,7 @@ require (
 	github.com/google/s2a-go v0.1.9 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.14 // indirect
-	github.com/googleapis/gax-go/v2 v2.18.0 // indirect
+	github.com/googleapis/gax-go/v2 v2.19.0 // indirect
 	github.com/gorilla/css v1.0.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
@@ -113,7 +113,7 @@ require (
 	google.golang.org/genproto v0.0.0-20260316180232-0b37fe3546d5 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20260316180232-0b37fe3546d5 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20260316180232-0b37fe3546d5 // indirect
-	google.golang.org/grpc v1.79.2 // indirect
+	google.golang.org/grpc v1.79.3 // indirect
 	google.golang.org/protobuf v1.36.11 // indirect
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
